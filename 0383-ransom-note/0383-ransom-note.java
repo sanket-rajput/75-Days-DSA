4a5a1deeb0ac10b59295f73ec3e76af22483c25e
Sanket Rajput <100849898+sanket-rajput@users.noreply.github.com>
2026-08-03 16:36:23 +0530

    Time: 15 ms (31.16%), Space: 47.4 MB (7.46%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+
+        if(ransomNote.length() > magazine.length()){
+            return false;
+        }
+        
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for(char c : magazine.toCharArray()){
+            map.put(c,map.getOrDefault(c, 0) + 1);
+        }
+        for(char c : ransomNote.toCharArray()){
+            if (!map.containsKey(c) || map.get(c) == 0) {
+                return false;
+            }
+            map.put(c, map.get(c) - 1);
+        }
+
+
+        return true;
+
+        
+    }
+}
